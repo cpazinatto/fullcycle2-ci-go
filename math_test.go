a331f08aecc6aca325aff2674e579bead17382be
Cássio Pazinatto <cassiopazinatto@gmail.com>
2021-02-24 19:54:49 -0300

    fix: fix a bug

--- math_test.go
+++ math_test.go
@@ -6,7 +6,7 @@ func TestSoma(t *testing.T) {
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da soma é inválido. Resultado é: %d. É esperado o número: %d.", total, 30)
+		t.Errorf("O resultado da soma é inválido. Resultado é: %d. É esperado o número: %d.", total, 30)
 	}
 
 }
